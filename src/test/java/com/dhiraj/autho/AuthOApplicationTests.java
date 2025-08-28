d0f29084a38c19625c8f521369dc0fece9bed1cf
Dhiraj Nagar <138846796+Dhirajmn@users.noreply.github.com>
2025-08-28 11:28:12 +0530

    Initial commit

---
+++ src/test/java/com/dhiraj/autho/AuthOApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhiraj.autho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthOApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
